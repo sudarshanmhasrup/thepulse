01ab873618fbb3881e8b2f1d7d8ac78b2963ac7f
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-07-14 18:11:07 +0530

    build: configure desktop JVM target with JVM 11 in build.gradle.kts

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -1,5 +1,15 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+}
+
+kotlin {
+    jvm("desktop") {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
+    }
 }
 
 layout.buildDirectory.set(file("$rootDir/.build/desktop"))
