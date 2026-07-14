5c109bc5ee7275637e8d66eee8ebb7d559a8ad29
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-07-14 13:41:15 +0530

    chore: initialize settings.gradle.kts file with initial basic gradle project setup

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+rootProject.name = "ThePulse"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
